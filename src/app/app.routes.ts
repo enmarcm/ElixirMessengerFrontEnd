03f4558167edb518e267a1fc6e220235d0887ab0
enmarcm <theenmanuel123@gmail.com>
2024-07-09 12:36:30 -0400

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,11 @@ export const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    
   },
   {
     path: 'main',
-    loadComponent: () => import('./main/main.page').then( m => m.MainPage)
+    loadComponent: () => import('./main/main.page').then( m => m.MainPage),
   },
   {
     path: "",
